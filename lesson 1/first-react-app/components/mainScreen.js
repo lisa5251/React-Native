167e114dfa7820805577628708c05789949daecb
lisa5251 <suliqlisa@gmail.com>
2025-11-06 20:47:48 +0100

    ndryshime

---
+++ lesson 1/first-react-app/components/mainScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Text, StyleSheet, View} from "react-native"
+
+function MainScreen(){
+    return(
+        <>
+        <div>Hello from main screen</div>
+        </>
+    )
+}
+
+export default MainScreen 
